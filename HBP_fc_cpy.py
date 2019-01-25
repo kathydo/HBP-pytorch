e722c1d61be3fd8c28d62ef9cde57a660a879f8f
Kathy Do <kathydo@p232-157.public.uvic.ca>
2019-01-25 15:32:45 -0800

    This is runnable on server

--- HBP_fc_cpy.py
+++ HBP_fc_cpy.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Fine-tune all layers only for HBP(Hierarchical Bilinear Pooling for Fine-Grained Visual Recognition).
+"""Fine-tune the fc layer only for  HBP(Hierarchical Bilinear Pooling for Fine-Grained Visual Recognition).
 Usage:
-    CUDA_VISIBLE_DEVICES=0,1 python HBP_all.py --base_lr 0.001 --batch_size 24 --epochs 200 --weight_decay 0.0005 --model 'HBP_fc_epoch_*.pth' | tee 'hbp_all.log'
+    CUDA_VISIBLE_DEVICES=0,1 python HBP_fc.py --base_lr 1.0 --batch_size 12 --epochs 120 --weight_decay 0.000005 | tee 'hbp_fc.log'
 """
-
 
 import os
 import torch
@@ -12,16 +11,16 @@ import torchvision
 import cub200
 import visdom
 import argparse
-vis = visdom.Visdom(env=u'HBP_all',use_incoming_socket=False)
+
+vis = visdom.Visdom(env=u'HBP_fc',use_incoming_socket=False)
 torch.manual_seed(0)
 torch.cuda.manual_seed_all(0)
 
 class HBP(torch.nn.Module):
     def __init__(self):
-        """Declare all needed layers."""
         torch.nn.Module.__init__(self)
         # Convolution and pooling layers of VGG-16.
-        self.features = torchvision.models.vgg16(pretrained=False).features
+        self.features = torchvision.models.vgg16(pretrained=True).features
         self.features_conv5_1 = torch.nn.Sequential(*list(self.features.children())
                                             [:-5])  
         self.features_conv5_2 = torch.nn.Sequential(*list(self.features.children())
@@ -34,6 +33,32 @@ class HBP(torch.nn.Module):
         # Linear classifier.
         self.fc = torch.nn.Linear(8192*3, 200)
 
+        # Freeze all previous layers.
+        for param in self.features_conv5_1.parameters():
+            param.requires_grad = False
+        for param in self.features_conv5_2.parameters():
+            param.requires_grad = False
+        for param in self.features_conv5_3.parameters():
+            param.requires_grad = False
+
+        # Initialize the fc layers.    
+        torch.nn.init.xavier_normal_(self.fc.weight.data)
+        if self.fc.bias is not None:
+            torch.nn.init.constant_(self.fc.bias.data, val=0)
+
+        #init
+        for m in self.bilinear_proj.modules():
+            if isinstance(m, torch.nn.Conv2d):
+                torch.nn.init.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    torch.nn.init.constant_(m.bias, 0)
+            elif isinstance(m, torch.nn.BatchNorm2d):
+                torch.nn.init.constant_(m.weight,1)
+                torch.nn.init.constant_(m.bias, 0)
+            elif isinstance(m, torch.nn.Linear):
+                torch.nn.init.xavier_normal_(m.weight)
+                torch.nn.init.constant_(m.bias, 0)
+
     def hbp(self,conv1,conv2):
         N = conv1.size()[0]
         proj_1 = self.bilinear_proj(conv1)
@@ -42,7 +67,7 @@ class HBP(torch.nn.Module):
         X = proj_1 * proj_2
         assert(X.size() == (N,8192,28,28))    
         X = torch.sum(X.view(X.size()[0],X.size()[1],-1),dim = 2)
-        X = X.view(N, 8192)   
+        X = X.view(N, 8192)     
         X = torch.sqrt(X + 1e-5)
         X = torch.nn.functional.normalize(X)
         return X
@@ -58,35 +83,35 @@ class HBP(torch.nn.Module):
         X_branch_2 = self.hbp(X_conv5_2,X_conv5_3)
         X_branch_3 = self.hbp(X_conv5_1,X_conv5_3)
 
-        X_branch = torch.cat([X_branch_1,X_branch_2,X_branch_3],dim=1)
+        X_branch = torch.cat([X_branch_1,X_branch_2,X_branch_3],dim = 1)
         assert X_branch.size() == (N,8192*3)
-
         X = self.fc(X_branch)
         assert X.size() == (N, 200)
         return X
 
 class HBPManager(object):
     def __init__(self, options, path):
-        print('Prepare the network and data.')
         self._options = options
         self._path = path
         # Network.
         self._net = torch.nn.DataParallel(HBP()).cuda()
         print(self._net)
-        self._net.load_state_dict(torch.load(self._path['model']))
         # Criterion.
         self._criterion = torch.nn.CrossEntropyLoss().cuda()
         # Solver.
         param_to_optim = []
         for param in self._net.parameters():
+            if param.requires_grad == False:
+                continue
             param_to_optim.append(param)
 
         self._solver = torch.optim.SGD(
             param_to_optim, lr=self._options['base_lr'],
             momentum=0.9, weight_decay=self._options['weight_decay'])
-        #milestones = [100]
-        #self._scheduler = torch.optim.lr_scheduler.MultiStepLR(self._solver,milestones = milestones,gamma=0.25)
-        self._scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self._solver, mode='max', factor=0.1, patience=5, verbose=True,threshold=1e-4)
+
+        milestones = [40,60,80,100]
+        self._scheduler = torch.optim.lr_scheduler.MultiStepLR(self._solver,milestones = milestones,gamma=0.25)
+
         train_transforms = torchvision.transforms.Compose([
             torchvision.transforms.Resize(size=448),  # Let smaller edge match
             torchvision.transforms.RandomHorizontalFlip(),
@@ -160,8 +185,8 @@ class HBPManager(object):
                 print('*', end='')
                 # Save model onto disk.
                 torch.save(self._net.state_dict(),
-                           os.path.join('./model/'
-                                        'HBP_all_epoch_%d.pth' % (t + 1)))
+                           os.path.join(self._path['model'],
+                                        'HBP_fc_epoch_%d.pth' % (t + 1)))
             print('%d\t%4.3f\t\t%4.2f%%\t\t%4.2f%%' %
                   (t+1, sum(epoch_loss) / len(epoch_loss), train_acc, test_acc))
         print('Best at epoch %d, test accuaray %f' % (best_epoch, best_acc))
@@ -203,7 +228,9 @@ class HBPManager(object):
         print(mean)
         print(std)
 
+
 def main():
+
     parser = argparse.ArgumentParser(
         description='Train HBP on CUB200.')
     parser.add_argument('--base_lr', dest='base_lr', type=float, required=True,
@@ -214,8 +241,6 @@ def main():
                         required=True, help='Epochs for training.')
     parser.add_argument('--weight_decay', dest='weight_decay', type=float,
                         required=True, help='Weight decay.')
-    parser.add_argument('--model', dest='model', type=str, required=True,
-                        help='Model for fine-tuning.')
     args = parser.parse_args()
     if args.base_lr <= 0:
         raise AttributeError('--base_lr parameter must >0.')
@@ -235,17 +260,16 @@ def main():
 
     project_root = os.popen('pwd').read().strip()
     path = {
-        'cub200': os.path.join('/data/datasets/birds'),
-        'model': os.path.join(project_root, 'model', args.model),
+        'cub200': '/data/datasets/birds',
+        'model': os.path.join(project_root, 'model'),
     }
     for d in path:
-        if d == 'model':
-            assert os.path.isfile(path[d])
-        else:
-            assert os.path.isdir(path[d])
+        print(d)
+        assert os.path.isdir(path[d])
 
     manager = HBPManager(options, path)
     manager.getStat()
     manager.train()
+
 if __name__ == '__main__':
     main()
